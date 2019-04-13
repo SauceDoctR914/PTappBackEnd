b0a43e79b61db27a00af3f40b4a1b150235edc4c
GavIn Sokolof <sokolofg@gmail.com>
2019-04-13 19:43:39 -0400

    post request working

--- src/routers/crud.js
+++ src/routers/crud.js
@@ -18,11 +18,8 @@ export const getOne = model => async (req, res) => {
 
 export const getMany = model => async (req, res) => {
   try {
-    const docs = await model
-      .find({})
-      .lean()
-      .exec();
-    return res.status(200).json({ data: docs });
+    const docs = await model.find({});
+    return res.status(200).send(docs);
   } catch (e) {
     console.error(e);
     res.status(400).send({ message: "unable to process" });
@@ -30,10 +27,10 @@ export const getMany = model => async (req, res) => {
 };
 
 export const createOne = model => async (req, res) => {
-  const createdBy = req.user._id;
+  const createdBy = req.body;
   try {
-    const doc = await model.create({ ...req.body, createdBy });
-    res.status(201).json({ data: doc });
+    const doc = await model.create(req.body);
+    res.status(201).send(doc);
   } catch (e) {
     console.error(e);
     res.status(400).end();
