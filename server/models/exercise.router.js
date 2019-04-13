d3f82bfaff0fb8b0a334f07aebc97495ec18dab5
GavIn Sokolof <sokolofg@gmail.com>
2019-04-13 14:54:04 -0400

    first

---
+++ server/models/exercise.router.js
@@ -0,0 +1,15 @@
+import controllers from "./item.controllers";
+import { Router } from "express";
+
+const router = Router();
+router
+  .route("/")
+  .get(controllers.getMany)
+  .post(controllers.createOne);
+router
+  .route("/:id")
+  .get(controllers.getOne)
+  .put(controllers.updateOne)
+  .delete(controllers.removeOne);
+
+export default router;
