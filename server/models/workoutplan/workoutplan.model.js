840fa17edf576dde86051ca323b53accf62a0f40
GavIn Sokolof <sokolofg@gmail.com>
2019-04-13 16:53:15 -0400

    chris workoutplan model

--- server/models/workoutplan/workoutplan.model.js
+++ server/models/workoutplan/workoutplan.model.js
@@ -1,1 +1,11 @@
 import mongoose from "mongoose";
+
+import exerciseSchema from ("../exercise/exercise.model");
+
+const workoutSchema = mongoose.Schema({
+  plan: {
+    type: [exerciseSchema]
+  }
+});
+
+export const Workout = mongoose.model('workout', workoutSchema);
