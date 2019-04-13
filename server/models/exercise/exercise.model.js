bb9ec33ab22c9ed57ab7d8f516cd18b9006405c6
GavIn Sokolof <sokolofg@gmail.com>
2019-04-13 16:54:18 -0400

    chris exercise model

---
+++ server/models/exercise/exercise.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const exerciseSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  reps: {
+    type: Number,
+    trim: true
+  },
+  duration: {
+    type: Number
+  }
+});
+
+export const Exercise = mongoose.model("exercise", exerciseSchema);
